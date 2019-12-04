6a98db34e8c1b72f097549ba4a19d5822ab41d15
bohdana999 <danushkaagneshka@gmail.com>
2019-12-04 23:08:07 +0200

    Створено найпростіший консольний додаток

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabMtzkit5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
